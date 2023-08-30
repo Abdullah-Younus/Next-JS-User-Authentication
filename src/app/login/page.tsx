49550c392ddf3a7490338b77a97868b167ac0f16
Muhammad Abdullah <abdullah1001k@gmail.com>
2023-08-30 20:19:00 +0500

    logout working

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,22 +1,52 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import axios from 'axios';
+import { toast } from 'react-hot-toast';
 
 const Loginpage = () => {
+
+    const router = useRouter();
     const [user, setUser] = React.useState({
         email: "",
         password: "",
     });
 
-    const onlogin = () => {
+    const [buttonDistabled, setButtonDisabled] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+
+    const onlogin = async () => {
+
+        try {
+            setLoading(true);
+            const res = await axios.post("/api/users/login", user);
+            console.log("Login successfully", res.data);
+            toast.success("login Success");
+            router.push("/profile");
+        } catch (error: any) {
+            console.log("login failed", error.message);
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
 
     }
 
+    useEffect(() => {
+        if (user.email.length > 0 && user.password.length > 0) {
+            setButtonDisabled(false);
+        } else {
+            setButtonDisabled(true);
+        }
+
+    }, [user])
+
     return (
         <div className='flex flex-col items-center justify-center min-h-screen py-2'>
-
+            <h1>{loading ? "Processing" : "Login"}</h1>
+            <hr />
             <label htmlFor="email">email</label>
             <input className='p-2 border border-gray-300 rounded-lg mb-2 focus:outline-none' type="text" id='email' value={user.email} onChange={(e) => setUser({ ...user, email: e.target.value })} />
 
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,34 @@
-import React from 'react'
+"use client"
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Link from 'next/link';
+import { Toast, toast } from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 
 const Profilepage = () => {
+
+  const router = useRouter();
+
+  const onLogout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success('logout successfully');
+      router.push("/login");
+    } catch (error: any) {
+      toast.error(error.message);
+    }
+  }
+
   return (
-    <div>Profile Page</div>
+    <div>
+      Profile Page
+      <hr />
+      <button onClick={onLogout} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4'>
+        Logout
+      </button>
+
+    </div>
   )
 }
 
