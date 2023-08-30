ab62c1702e23e9b1c879e677e800eccd2d71fa02
Muhammad Abdullah <abdullah1001k@gmail.com>
2023-08-30 18:20:02 +0500

    local changs

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import axios from 'axios';
+import { toast } from 'react-hot-toast';
 
 const Signup = () => {
 
@@ -13,15 +14,27 @@ const Signup = () => {
         username: "",
     });
     const [buttonDistabled, setButtonDisabled] = React.useState(false);
+    const [loading, setLoading] = useState(false);
+
 
     const onSignUp = async () => {
-
+        try {
+            setLoading(true);
+            const response = await axios.post('/api/users/signup', user)
+            console.log("Signup Success", response.data);
+            router.push("/login");
+        } catch (error: any) {
+            console.log(error);
+            toast.error(error.message);
+        } finally {
+            setLoading(false);
+        }
     }
 
     useEffect(() => {
         if (user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
             setButtonDisabled(false);
-        }else  {
+        } else {
             setButtonDisabled(true);
         }
     }, [user])
@@ -29,7 +42,7 @@ const Signup = () => {
 
     return (
         <div className='flex flex-col items-center justify-center min-h-screen py-2'>
-            <h1 className='text-center text-black text-2xl'>Signup</h1>
+            <h1 className='text-center text-black text-2xl'>{loading ? "Processing" : "Signup"}</h1>
             <label htmlFor="username">username</label>
             <input className='p-2 border border-gray-300 rounded-lg mb-2 focus:outline-none' type="text" id='username' value={user.username} onChange={(e) => setUser({ ...user, username: e.target.value })} />
             <label htmlFor="email">email</label>
@@ -38,7 +51,7 @@ const Signup = () => {
             <label htmlFor="password">password</label>
             <input className='p-2 border border-gray-300 rounded-lg mb-2 focus:outline-none' type="password" id='password' value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })} />
 
-            <button onClick={onSignUp}>SignUp Here</button>
+            <button className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600' onClick={onSignUp}>{buttonDistabled ? "No Signup" : "Signup"}</button>
             <Link href={"/"}>Visit to login page</Link>
         </div>
     )
